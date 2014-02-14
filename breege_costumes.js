6382e4cd2eab13d607dfa73e6441154d6140c762
PJ Dunlevy <yorko9999@gmail.com>
2014-02-14 12:53:51 +0000

    First commit

---
+++ breege_costumes.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+							   
+		var currentPosition = 0;
+		var slideWidth = 640;
+		var slides = $('.slide');
+		var numberOfSlides = slides.length;
+		var slideShowInterval;
+		var speed = 5000;
+
+		
+		
+		slides.wrapAll('<div id="slidesHolder"></div>')
+		
+		slides.css({ 'float' : 'left' });
+		
+		//set #slidesHolder width equal to the total width of all the slides
+		$('#slidesHolder').css('width', slideWidth * numberOfSlides);
+
+		
+		manageNav(currentPosition);
+		
+		//tell the buttons what to do when clicked
+		$('.nav').on('click', function() {
+			
+			//determine new position
+			currentPosition = ($(this).attr('id')=='arrow_right') ? currentPosition+1 : currentPosition-1;
+										
+			//hide/show controls
+			manageNav(currentPosition);
+			clearInterval(slideShowInterval);
+			changePosition;
+			moveSlide();
+		});
+		
+		function manageNav(position) {
+			//hide left arrow if position is first slide
+			if(position==0){ $('#arrow_left').hide() }
+			else { $('#arrow_left').show() }
+			//hide right arrow is slide position is last slide
+			if(position==numberOfSlides-1){ $('#arrow_right').hide() }
+			else { $('#arrow_right').show() }
+		}
+
+		
+		/*changePosition: this is called when the slide is moved by the 
+        timer and NOT when the next or previous buttons are clicked*/
+		function changePosition() {
+			if(currentPosition == numberOfSlides - 1) {
+				currentPosition = 0;
+				manageNav(currentPosition);
+			} else {
+				currentPosition++;
+				manageNav(currentPosition);
+			}
+			moveSlide();
+		}
+		
+		
+		//moveSlide: this function moves the slide 
+		function moveSlide() {
+				$('#slidesHolder')
+                  .animate({'marginLeft' : slideWidth*(-currentPosition)});
+		}
+
+	});
