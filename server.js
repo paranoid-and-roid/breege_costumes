a916ee13e424d1e92410051f552e43675ca57400
PJ Dunlevy <yorko9999@gmail.com>
2014-10-23 14:28:25 +0100

    New js and css folders added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express 	= 	require('express'),
 app.use(bodyParser.urlencoded({	extended: true }));
 app.use(bodyParser.json());
 
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/css', express.static(__dirname + '/css'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/images', express.static(__dirname + '/images'));
 app.use('/templates', express.static(__dirname + '/templates'));
